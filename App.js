02480f780eccbff67e5dd832ba4f06b488def9e9
Vladimir Copa Fabian <vcopafabian@gmail.com>
2022-10-04 15:17:42 -0400

    plantilla login

--- App.js
+++ App.js
@@ -25,8 +25,34 @@ const config = {
 export const theme = extendTheme({ config });
 
 export default function App() {
+  const theme = extendTheme({
+    colors: {
+      // Add new color
+      primary: {
+        50: '#f5f3ff',
+        100: '#ede9fe',
+        200: '#ddd6fe',
+        300: '#c4b5fd',
+        400: '#a78bfa',
+        500: '#8b5cf6',
+        600: '#7c3aed',
+        700: '#6d28d9',
+        800: '#5b21b6',
+        900: '#4c1d95',
+      },
+      // Redefining only one shade, rest of the color will remain same.
+      amber: {
+        400: '#d97706',
+      },
+    },
+    config: {
+      // Changing initialColorMode to 'dark'
+      initialColorMode: 'dark',
+    },
+  });
+
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <MainStack />
       {/* <Center
         _dark={{ bg: "blueGray.900" }}
--- src/views/Login.component.js
+++ src/views/Login.component.js
@@ -1,53 +1,85 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-/* import { Icon, Input, Layout, ViewPager } from '@ui-kitten/components'; */
-/* import { Text } from '@ui-kitten/components'; */
-/* import { TouchableWithoutFeedback } from '@ui-kitten/components/devsupport'; */
 import { LayoutTemplante } from '../components/Layout.templante.component';
-import { Text } from "native-base";
+import { Box, Button, Center, FormControl, Icon, Input, Stack, Text, WarningOutlineIcon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 export const Login = ({ navigation }) => {
 
+    const [showPassword, setShowPassword] = React.useState(false);
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
-    const [secureTextEntry, setSecureTextEntry] = React.useState(true);
-/*     const renderInputIcon = (props) => (
-        <TouchableWithoutFeedback onPress={toggleSecureEntry}>
-            <Icon {...props} name={!secureTextEntry ? 'eye' : 'eye-off'} />
-        </TouchableWithoutFeedback>
-    ); */
-    /* const toggleSecureEntry = () => {
-        setSecureTextEntry(!secureTextEntry);
-    }; */
+    const [password1, setPassword1] = React.useState('');
     return (
         <LayoutTemplante
-            header={()=>
-                <Text>
-                    ert
-                </Text>
-            }
-            component={()=>
-                <React.Fragment>
-                    <Text /* style={styles.text} */ category='h4'>¡Bienvenidos!</Text>
-                    <Text /* style={styles.text} */ category='s2' appearance='hint'>Inicia sesión para continuar</Text>
-                    {/* <Icon name={'email'} style={styles.icon} fill='#8F9BB3'/> */}
-                    {/* <Input
-                        accessoryLeft={EmailIcon}
-                        style={styles.input}
-                        placeholder='Correo electrónico'
-                        value={email}
-                        onChangeText={setEmail}
-                    /> */}
+            
+            component={() =>
+                <Box>
+                    <Text fontSize="xl" bold color={'black'}>¡Bienvenidos!</Text>
+                    <Text color={'text.400'} >Inicia sesión para continuar</Text>
+                    <Stack direction="row" mb="2.5" mt="4" space={4} width={'full'}>
+                        <Center width={'15%'}>
+                            <Box bg="primary.50" alignItems="center"
+                                p="2" rounded="xl">
+                                <Icon as={<MaterialIcons name="email" />} size={6} color="primary.800" marginTop={2} marginBottom={2} />
+                            </Box>
+                        </Center>
+                        <Center width={'80%'} >
+                        <FormControl>
+                                <FormControl.Label>
+                                <Text fontSize="xs" bold color={'dark.300'}>CORREO ELECTRÓNICO</Text>
+                                </FormControl.Label>
+                                <Input variant="underlined" borderBottomColor={'dark.700'} color={'black'} value={email} onChangeText={text => setEmail(text)} />
+                                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                                    Try different from previous passwords.
+                                </FormControl.ErrorMessage>
+                            </FormControl>
+                        </Center>
+                    </Stack>
+                    <Stack direction="row" mb="2.5" mt="1.5" space={4}>
+                        <Center width={'15%'}>
+                            <Box bg="primary.50" alignItems="center"
+                                p="2" rounded="xl">
+                                <Icon as={<MaterialIcons name="keyboard" />} size={6} color="primary.800" marginTop={2} marginBottom={2} />
+                            </Box>
+                        </Center>
+                        <Center width={'80%'}>
+                            <FormControl>
+                                <FormControl.Label><Text fontSize="xs" bold color={'dark.300'}>CONSTRASEÑA</Text></FormControl.Label>
+                                <Input variant="underlined" placeholder="" color={'black'} type={showPassword ? "text" : "password"}
+                                    InputRightElement={<Pressable onPress={() => setShowPassword(!showPassword)}><Icon as={<MaterialIcons name={showPassword ? "visibility" : "visibility-off"} />}
+                                        size={4} mr="2" color="muted.400" /></Pressable>}
+                                    borderBottomColor={'dark.700'}
+                                    value={password} onChangeText={setPassword}
+                                    onch
+                                />
+                                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                                    Try different from previous passwords.
+                                </FormControl.ErrorMessage>
+                            </FormControl>
+                        </Center>
 
-                    {/* <Input
-                        style={styles.input}
-                        accessoryLeft={KeyIcon}
-                        placeholder='Contraseña'
-                        value={password}
-                        secureTextEntry={secureTextEntry}
-                        onChangeText={setPassword}
-                        accessoryRight={renderInputIcon}
-                    /> */}
-                </React.Fragment>
+                        
+
+                    </Stack>
+                    <Button><Text bold>Iniciar sesión</Text></Button>
+                    <Box alignItems={'center'}>
+                    <Text color={'dark.500'} marginTop={6}>
+                        ¿Olvidaste tu contraseña?
+                    </Text>
+
+                    <Text color={'dark.500'} marginTop={4}>
+                        ¿Nuevo usuario? {' '}
+                        <Text  color={'orange.400'} bold>
+                        Registrate
+                    </Text>
+                    </Text>
+
+
+
+
+                    </Box>
+                </Box>
             }
         />
     );
